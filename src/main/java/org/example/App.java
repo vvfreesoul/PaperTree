75c74a5dbae7c655a0d4185beed05a592e818ce7
guowei <285009003@qq.com>
2021-01-30 21:08:18 +0800

    class tree

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,38 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.*;
+
 /**
  * Hello world!
  *
  */
-public class  App
+public class  App<i>
 {
+    Tree TRSTree(List<Map<Integer, Integer>> avg_host,List<Map<Integer, Integer>> avg_data,int fanout)
+    {
+
+    }
     public static void main( String[] args )
     {
+        List<Map<Integer, Integer>> avg_host=new ArrayList<>();
+        List<Map<Integer, Integer>> avg_data=new ArrayList<>();
+//        Random random = new Random();
+//        Random random2 = new Random();
+//        System.out.println(random.nextInt((128)));
+//        System.out.println(random.nextInt((128)));
+
+        for(int i=0;i<100000;i++)
+        {
+            Random random = new Random();
+            Map<Integer, Integer> map=new HashMap<Integer, Integer>();
+            map.put(i,random.nextInt(128));
+            avg_host.add(map);
+            Map<Integer, Integer> map_data=new HashMap<Integer, Integer>();
+            map_data.put(i,2*random.nextInt(128));
+            avg_data.add(map_data);
+        }
+
         Tree tree = new Tree();
         tree.add(0, "一级树杈1");
         tree.add(0, "一级树杈2");
--- src/main/java/org/example/NodeLeaf.java
+++ src/main/java/org/example/NodeLeaf.java
@@ -3,18 +3,16 @@ package org.example;
 import java.util.*;
 import java.util.ArrayList;
 
-class Node{
+class NodeLeaf{
     private int Id;    //节点Id
     private int level;   //肯定要记录层次
     private int type;    //要记录是路由节点还是映射节点，映射节点保存公式，保存离群值
-    private int alpha;
-    private int beta;
     private String data; //节点数据,这里应该保存一个公式
     public List<Node> nodes = new ArrayList<Node>(); //多个子节点，利用List实现
-    public Node(int Id){
+    public NodeLeaf(int Id){
         this.Id = Id;
     }
-    public Node(int Id,String data){
+    public NodeLeaf(int Id,String data){
         this.Id = Id;
         this.data = data;
     }
